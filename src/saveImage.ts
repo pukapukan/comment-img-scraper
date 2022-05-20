c59e07dd96b936b2677587fd133b8f57ac44dc48
Jason Park <jpar225@gmail.com>
2022-05-20 11:57:35 -0700

    saveImage returns filepath instead of filename

--- src/saveImage.ts
+++ src/saveImage.ts
@@ -3,21 +3,21 @@ import fs from 'fs'
 
 const fileExtensionPattern = /filename=(?<extension>\S*)/g
 
-export default async (postingNo: string | number, imageUrl: string) => {
+export default async (postingNo: string | number, imageUrl: string): Promise<string> => {
     const response = await axios.get(imageUrl, { responseType: 'stream' })
     const contentDisposition = response.headers['content-disposition']
     const match = fileExtensionPattern.exec(contentDisposition)
     if (!match) {
         throw new Error('Unable to get image extension from HTTP response')
     }
-    const filename = match[1]
-
     const directory = `img/${postingNo}`
     if (!fs.existsSync(directory)) {
         fs.mkdirSync(directory, { recursive: true })
     }
+    const filename = match[1]
+    const filepath = `${directory}/${filename}`
 
-    response.data.pipe(fs.createWriteStream(`${directory}/${filename}`))
+    response.data.pipe(fs.createWriteStream(filepath))
     return filename
 }
 
